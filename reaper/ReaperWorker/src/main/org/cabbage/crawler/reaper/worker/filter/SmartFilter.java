5924b70f138279ec3bd21053e21518ce3a8320e9
bennysen <64755493@qq.com>
2018-06-19 20:41:35 +0800

    add ReaperWorker

---
+++ reaper/ReaperWorker/src/main/org/cabbage/crawler/reaper/worker/filter/SmartFilter.java
@@ -0,0 +1,16 @@
+package org.cabbage.crawler.reaper.worker.filter;
+
+/**
+ * 自动过滤器
+ * 
+ * @author wkshen
+ *
+ */
+public abstract class SmartFilter extends AbstractFilter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
